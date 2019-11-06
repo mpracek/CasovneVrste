018f224e2139085b1eee0b3aa7df8953b70248c3
mpracek299@gmail.com <mpracek299@gmail.com>
2019-11-06 19:30:43 +0100

    dodatki

---
+++ napakax32.py
@@ -0,0 +1,19 @@
+[Running] python -u "c:\Users\Lenovo_Uporabnik\Desktop\FMF\Magisterij\Casovne_vrste\koda.py"
+Traceback (most recent call last):
+  File "c:\Users\Lenovo_Uporabnik\Desktop\FMF\Magisterij\Casovne_vrste\koda.py", line 4, in <module>
+    from keras.callbacks import LambdaCallback
+  File "C:\ProgramData\lib\site-packages\keras\__init__.py", line 3, in <module>
+    from . import utils
+  File "C:\ProgramData\lib\site-packages\keras\utils\__init__.py", line 2, in <module>
+    from . import np_utils
+  File "C:\ProgramData\lib\site-packages\keras\utils\np_utils.py", line 6, in <module>
+    import numpy as np
+  File "C:\ProgramData\lib\site-packages\numpy\__init__.py", line 140, in <module>
+    from . import _distributor_init
+  File "C:\ProgramData\lib\site-packages\numpy\_distributor_init.py", line 26, in <module>
+    WinDLL(os.path.abspath(filename))
+  File "C:\ProgramData\lib\ctypes\__init__.py", line 344, in __init__
+    self._handle = _dlopen(self._name, mode)
+OSError: [WinError 193] %1 ni veljaven program Win32
+
+[Done] exited with code=1 in 3.505 seconds
---
+++ prva_domaca.py
@@ -0,0 +1,20 @@
+#prva domaca naloga
+#Martin Pracek 
+#115 120
+
+
+#1) LSTMs on paper
+# Firstly, we import the text and set indices to characters.
+# In second step, algortihm dissetcs text into sequences of the same length "maxlen"
+# Vectorization begins by giving us two arrays full of Booleans. 
+
+
+#
+
+#2) Seeding, Temperature and Creativity
+
+#3)Layer Definition
+
+#4) Quality, Vocabulary and Explainability
+
+#5) CNN Variant
